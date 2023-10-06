6ac77af853da69b750e81cec8d6221320ddb2a26
JankerMLD <serebr.s2012@gmail.com>
2023-10-06 16:08:28 +0300

    renamed Stepik.py to is_pangram. moved books for reading to lenrning projects

---
+++ books_for_reading.py
@@ -0,0 +1,4 @@
+n, m, k, x, y, z, t, a = set([int(input())]), set([int(input())]), set([int(input())]), set([int(input())]), set([int(input())]), set([int(input())]), set([int(input())]), set([int(input())])
+result1 = n ^ m ^ k
+result2 = k | y | z
+result3 = 
