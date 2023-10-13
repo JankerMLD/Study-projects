bf610c8d93868122690e02f782b014a3ca51f5c2
JankerMLD <serebr.s2012@gmail.com>
2023-10-13 13:31:07 +0300

    йуайуАП

--- books_for_reading.py
+++ books_for_reading.py
@@ -1,4 +1,10 @@
-n, m, k, x, y, z, t, a = set([int(input())]), set([int(input())]), set([int(input())]), set([int(input())]), set([int(input())]), set([int(input())]), set([int(input())]), set([int(input())])
-result1 = n ^ m ^ k
-result2 = k | y | z
-result3 = 
+n, m, k, x, y, z, t, a = int(input()),  int(input()),  int(input()),  int(input()),  int(input()),  int(input()),  int(input()), int(input())
+o = n + m - x
+u = m + k - y
+e = k + n - z
+result1 = (n - o - e + t) + (m - u - o + t) + (k - u - e + t)
+result2 = o + u + e - 3 * t
+result3 = a - (x + k - u - e + t)
+print(result1)
+print(result2)
+print(result3)
